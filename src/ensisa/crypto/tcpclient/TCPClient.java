61ab73a320e6d40b3333bbcd27f374541273d830
sebastien <sebastien@ip-83-141-174-111.numericable.fr>
2014-12-16 12:55:27 +0100

    changement of TCPServer/TCPClient, adding all Reader/Writer, removing CA class from TCPServer adding in TCPCommon

--- src/ensisa/crypto/tcpclient/TCPClient.java
+++ src/ensisa/crypto/tcpclient/TCPClient.java
@@ -1,34 +1,52 @@
 package ensisa.crypto.tcpclient;
 
+import ensisa.crypto.tcpcommon.PROTOCOL;
+import java.io.IOException;
 import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class TCPClient 
-{
+public class TCPClient {
 
-    private Socket connection = null;
-    
-    public void connect(String serverName)
-    {
-        
-    }
-    
-    public void ftp(String serverName)
-    {
-        connect(serverName);
-        Session session = new Session(connection);
-        while(true)
-        {
-            
-        }
-    }
-    
+    private Socket connection;
+  
     /**
      * 
+     */
+    public TCPClient(String serverName) {
+        try {
+            this.connect(serverName);
+        } catch (IOException ex) {
+            Logger.getLogger(TCPClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * initialize the socket
+     */
+    public void connect(String serverName) throws IOException {
+        connection = new Socket(serverName, PROTOCOL.PORT);
+    }
+    
+    @Deprecated
+    //should be in Session
+    public void ftp() {
+        try {
+            ClientReader reader;
+            reader = new ClientReader(connection.getInputStream());
+            reader.receive();
+        } catch (IOException ex) {
+            Logger.getLogger(TCPClient.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    /**
+     *
      * @param args les (éventuels) arguments passés en ligne de commande
      */
-    
-    public static void main(String[] args)
-    {
-        
+    public static void main(String[] args) {
+        TCPClient client = new TCPClient("localhost");
+        client.ftp();
     }
 }
